ca1b083476c00ee202b9db471bfeb219ff335cbd
Paulo Eduardo Correia <correia97@gmail.com>
2024-11-08 22:17:59 -0300

    feat:Configuração dos ambientes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { environment } from '../environments/environment';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +11,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
+  nome = environment.nome;
   title = 'exemplo';
 }
